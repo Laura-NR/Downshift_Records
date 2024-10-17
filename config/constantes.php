9d80d9a86f8083e48791ebe4d0aff603c01e92c3
Laura Nunez <lenrodriguez@iutbayonne.univ-pau.fr>
2024-10-17 12:11:20 +0200

    Création des constantes

---
+++ config/constantes.php
@@ -0,0 +1,14 @@
+<?php 
+
+//Définition des constantes pour la connexion à la base de données
+define('DB_HOST', 'http://lakartxela.iutbayonne.univ-pau.fr/');
+define('DB_NAME', 'ldhildevert_bd');
+define('DB_USER', 'ldhildevert_bd');
+define('DB_PASSWORD', 'ldhildevert_bd');
+
+//Définition des contantes pour les tables
+define('PREFIX', 'redord_');
+
+//Constantes pour la partie vue
+define('WEBSITE_TITLE', 'Downshift Records');
+define('WEBSITE_LANGUAGE', 'fr');
