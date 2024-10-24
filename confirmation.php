6259ad23432e24f5388044fd3d5115fcbe461032
Laura Nunez <lenrodriguez@iutbayonne.univ-pau.fr>
2024-10-24 21:34:44 +0200

    CSS de la page de confirmation du paiement

--- confirmation.php
+++ confirmation.php
@@ -19,40 +19,33 @@ if (isset($_POST['total'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="CSS/styles.css">
-    <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.js"></script>
-    <!-- <link rel='stylesheet' type='text/css' href='style.css'> -->
     <title><?= constant('WEBSITE_TITLE') ?></title>
 </head>
 
-<body class="container">
+<body>
     <header>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="index.php"><?= constant('WEBSITE_TITLE') ?></a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="index.php">CDs</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="panier.php">Panier</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="logout.php">Déconnexion</a>
-                        </li>
-                    </ul>
-                </div>
+        <nav class="header_menu">
+            <a class="navbar-brand" href="index.php"><?= constant('WEBSITE_TITLE') ?></a>
+            <div class="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.php">CDs</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="panier.php">Panier</a>
+                    </li>
+                    <li class="nav-item" id="logout_btn">
+                        <a class="nav-link" href="logout.php">Déconnexion</a>
+                    </li>
+                </ul>
             </div>
         </nav>
     </header>
 
-    <main>
-        <h1 class="">Confirmation du Payment :</h1>
+    <main class="container" id="confirm_payment" style="min-height: 70vh;">
+        <h1 class="page_title">Confirmation du Payment :</h1>
 
-        <h3>Montant total à payer : <?= $total ?></h3>
+        <h3 style="margin-bottom: 50px;">Montant total à payer : <?= $total ?></h3>
 
         <form action="payment.php" method="post">
             <label for="carte_bleu">Numero de carte :</label>
