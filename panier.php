a1550c34253c8a7fed3526f8e84f7bb289e63b37
Laura Nunez <lenrodriguez@iutbayonne.univ-pau.fr>
2024-10-24 21:23:09 +0200

    CSS de la page du panier done

--- panier.php
+++ panier.php
@@ -19,37 +19,30 @@ $total = 0;
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="CSS/styles.css">
-    <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.js"></script>
-    <!-- <link rel='stylesheet' type='text/css' href='style.css'> -->
     <title><?= constant('WEBSITE_TITLE') ?></title>
 </head>
 
-<body class="container">
+<body>
     <header>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="index.php"><?= constant('WEBSITE_TITLE') ?></a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="index.php">CDs</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="panier.php">Panier</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="logout.php">Déconnexion</a>
-                        </li>
-                    </ul>
-                </div>
+        <nav class="header_menu">
+            <a class="navbar-brand" href="index.php"><?= constant('WEBSITE_TITLE') ?></a>
+            <div class="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.php">CDs</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="panier.php">Panier</a>
+                    </li>
+                    <li class="nav-item" id="logout_btn">
+                        <a class="nav-link" href="logout.php">Déconnexion</a>
+                    </li>
+                </ul>
             </div>
         </nav>
     </header>
 
-    <main>
+    <main class="container" style="min-height: 70vh;">
         <h1>Voici votre panier :</h1>
         <table>
             <thead>
@@ -69,7 +62,7 @@ $total = 0;
                 <?php else: ?>
                     <?php foreach ($panier as $cd_id => $item): ?>
                         <tr>
-                            <td><img src="images/<?= $item['vignette'] ?>" alt="<?= htmlspecialchars($item['titre']) ?>"><?= htmlspecialchars($item['titre']) ?></td>
+                            <td id="img_title"><img src="images/<?= $item['vignette'] ?>" alt="<?= htmlspecialchars($item['titre']) ?>"><?= htmlspecialchars($item['titre']) ?></td>
                             <td><?= number_format(floatval($item['prix']), 2) ?> €</td>
                             <td>
                                 <form action="quantity.php" method="post">
@@ -90,20 +83,23 @@ $total = 0;
                     <?php endforeach; ?>
                 <?php endif; ?>
             </tbody>
-
+        </table>
+        <div id="total_payer">
             <h3>Total à payer: <?= number_format($total, 2) ?> €</h3>
 
             <form action="confirmation.php" method="post">
                 <input type="hidden" name="total" value="<?= number_format($total, 2) ?>">
                 <input type="submit" value="Confirmer le payment">
             </form>
-
-            <a href="index.php">Continuer à acheter</a>
+        </div>
+        <div class="return_container">
+            <a href="index.php" class="return_btn">Continuer à acheter</a>
+        </div>
     </main>
-    <footer class="text-body-secondary py-5">
+
+    <footer>
         <p>&copy; <?= constant('WEBSITE_TITLE') ?> - <?= date('Y') ?></p>
     </footer>
-
 </body>
 
 </html>
